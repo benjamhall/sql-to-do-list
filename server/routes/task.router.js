e4c9a9abdfa82ae8c24c892adc75deca2473da64
Ben Hall <benjamhall@gmail.com>
2021-05-28 17:00:26 -0500

    database.sql

--- server/routes/task.router.js
+++ server/routes/task.router.js
@@ -1,77 +1,77 @@
-// const express = require('express');
-// const koalaRouter = express.Router();
-// const pool = require('../modules/pool');
+const express = require('express');
+const router = express.Router();
+const pool = require('../modules/pool');
 
-// // DB CONNECTION
+// DB CONNECTION
 
 
-// //! GET
-// koalaRouter.get('/', (req, res) => {
-//     // this will tell SQL that we want all koalas in the database and to sort by it's id number
-//     let queryText = 'SELECT * FROM "koalas" ORDER BY "id";';
-//     pool.query(queryText).then(result => {
-//     // sends back the results in an object
-//     res.send(result.rows);
-//     }).catch(error => {
-//         console.log('error getting koalas', error);
-//         res.sendStatus(500);
-//     });
-// });
+//! GET
+router.get('/', (req, res) => {
+    // this will tell SQL that we want all koalas in the database and to sort by it's id number
+    let queryText = 'SELECT * FROM "koalas" ORDER BY "id";';
+    pool.query(queryText).then(result => {
+    // sends back the results in an object
+    res.send(result.rows);
+    }).catch(error => {
+        console.log('error getting koalas', error);
+        res.sendStatus(500);
+    });
+});
 
-// // POST
-// koalaRouter.post('/', (req, res) => {
-// // console log it to make sure it received from client
-//     console.log('req.body is ...', req.body);
+// POST
+router.post('/', (req, res) => {
+// console log it to make sure it received from client
+    console.log('req.body is ...', req.body);
 
-// // need to specify what we are sending to sql in sql terms and sanitize it
-//     let queryText = `INSERT INTO "koalas" ("name", "gender", "age", "ready_to_transfer", "notes") 
-//     VALUES ($1, $2, $3, $4, $5);`;
+// need to specify what we are sending to sql in sql terms and sanitize it
+    let queryText = `INSERT INTO "koalas" ("name", "gender", "age", "ready_to_transfer", "notes") 
+    VALUES ($1, $2, $3, $4, $5);`;
 
-// // insert values from user object now that it has been sanitized
-//     let values = [req.body.name, req.body.gender, req.body.age, req.body.readyForTransfer, req.body.notes];
+// insert values from user object now that it has been sanitized
+    let values = [req.body.name, req.body.gender, req.body.age, req.body.readyForTransfer, req.body.notes];
 
-// // send the information to the database
-// pool.query(queryText, values)
-//     .then( result => {
-//         console.log('Response from sql', result);
-//         res.sendStatus(201);
-//     }).catch( err => {
-//         console.log('The POST route server error is', err);
-//         res.sendStatus(500);
-//     });
+// send the information to the database
+pool.query(queryText, values)
+    .then( result => {
+        console.log('Response from sql', result);
+        res.sendStatus(201);
+    }).catch( err => {
+        console.log('The POST route server error is', err);
+        res.sendStatus(500);
+    });
 
-// });
+});
 
-// // PUT
+// PUT
 
-// koalaRouter.put('/:id', (req, res) => {
-//     const koalaId = req.params.id;
+koalaRouter.put('/:id', (req, res) => {
+    const koalaId = req.params.id;
 
-//     //Change the status of the koala to mark ready
-//     let koalaReady = req.body.koalaReady;
+    //Change the status of the koala to mark ready
+    let koalaReady = req.body.koalaReady;
 
-//     let queryString = '';
+    let queryString = '';
 
-//     if (koalaReady === true) {
-//         queryString = `UPDATE "koalas" SET "ready_to_transfer" = NOT "ready_to_transfer" WHERE "koala".id = $1;`;
-//     } else {
-//         // If the koalaReady is somehow not what we expect, we reject the response and send
-//         // back a 500 error
-//         res.sendStatus(500);
-//         return; //early exit since it's an error!
-//     }
-//     pool.query(queryString, [koalaId])
-//         .then(response => {
-//             console.log(response.rowCount);
-//             res.sendStatus(202)
-//         })
-//         .catch(err => {
-//             console.log('This is frustrating', err);
-//             res.sendStatus(500);
-//         })
+    if (koalaReady === true) {
+        queryString = `UPDATE "koalas" SET "ready_to_transfer" = NOT "ready_to_transfer" WHERE "koala".id = $1;`;
+    } else {
+        // If the koalaReady is somehow not what we expect, we reject the response and send
+        // back a 500 error
+        res.sendStatus(500);
+        return; //early exit since it's an error!
+    }
+    pool.query(queryString, [koalaId])
+        .then(response => {
+            console.log(response.rowCount);
+            res.sendStatus(202)
+        })
+        .catch(err => {
+            console.log('This is frustrating', err);
+            res.sendStatus(500);
+        })
 
-// })
+})
 
-// // DELETE
+// DELETE
 
-// module.exports = koalaRouter;
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
-const koalaRouter = require('./routes/task.router.js')
+const router = require('./routes/task.router.js')
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
 
 // ROUTES
-app.use('/tasks', taskRouter)
+app.use('/tasks', router)
 
 // Start listening for requests on a specific port
 app.listen(PORT, () => {
